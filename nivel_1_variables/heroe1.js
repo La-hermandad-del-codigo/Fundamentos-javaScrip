05dea2d9810cf98656ac6e7edd3b54f85d4cd02f
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 09:04:02 -0500

    edicion de heroe 2

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -5,3 +5,7 @@ let ciudad = "Tokyo"
 let enemigo = "All for one"
 
 console.log(`${nombreHeroe} tiene ${poder} de poder, pero tiene ${nivelEnergia} de energia y se encuentra peleando con ${enemigo} en ${ciudad}`);
+
+nivelEnergia = 30;
+
+console.log(`${nombreHeroe} se canso por pelear mucho y su energia bajo a ${nivelEnergia}`)
