2f77726f76235f74c49805731c1d790846084cda
Gustavo Tuesta <gustavo.tuesta.puse@gmail.com>
2026-02-28 13:03:53 -0500

    avance restaurante

--- nivel_3_objetos/restaurante.js
+++ nivel_3_objetos/restaurante.js
@@ -1,18 +1,31 @@
 let cliente = {
-    nombre : "Carlitos",
-    dinero : 70,
-    hambre : 60
+    nombre: "Carlitos",
+    dinero: 200,
+    hambre: 60
+}
+
+let comida = {
+    nombre: "Plancha de Makis",
+    precio: 150,
+    satisfaccion: 50
 }
 
 
-function ordenarComida(nom, com, pre) {
-    
+function ordenarComida(cliente, comida) {
+    console.log(`${cliente.nombre} pidio un(a) ${comida.nombre} el cual cuesta ${comida.precio} soles.`)
+    cliente.hambre -= comida.satisfaccion;
+    pagar(cliente, comida);
+
 }
 
-function pagar(pre, dinero) {
-
+function pagar(cliente, comida) {
+    console.log(`${cliente.nombre} tiene que pagar ${comida.precio} soles, pero lo hace con ${cliente.dinero} soles.`)
+    cliente.dinero -= comida.precio;
+    mostrarEstado(cliente)
 }
 
-function mostrarEstado(clien) {
-    
+function mostrarEstado(cliente) {
+    console.log(`${cliente.nombre} se quedo con ${cliente.dinero} soles y con ${cliente.hambre} de hambre.`)
 }
+
+ordenarComida(cliente, comida)
