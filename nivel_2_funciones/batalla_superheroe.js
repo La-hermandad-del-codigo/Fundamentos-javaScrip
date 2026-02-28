f5e7e1e9a5f8b7150bf637ddcf4df282a0517c40
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 10:56:42 -0500

    simulacion de batalla

--- nivel_2_funciones/batalla_superheroe.js
+++ nivel_2_funciones/batalla_superheroe.js
@@ -1,20 +1,34 @@
+// gustavo
 let nombreHeroe1 = "All Might";
 let poderHeroe1 = 89;
 let nivelEnergiaHeroe1 = 50;
-let daño = 0;
+let daño1 = 6;
 let defensaHeroe1 = 4;
 
-function ataque() {
-    daño = Math.floor(Math.random() * 10) + 1; //Para generar un daño aleatorio entre 1 y 10
+//Jhefry
+let nombreHeroe2 = "Frieren";
+let poderHeroe2 = 85;
+let nivelEnergiaHeroe2 = 98;
+let daño2 = 6;
+let defensaHeroe2 = 3;
+
+
+let energiaActual = 0;
+
+
+function ataque(nombre1, nombre2, ataque, energia, recuperacion) {
+    console.log(`${nombre2} ataco a ${nombre1}`)
+    defender(nombre1,nombre2, ataque, energia, recuperacion)
 }
 
-function defender(daño, defensa) {
-    if (daño > defensa) {
-        nivelEnergia -= daño;
-        console.log(nivelEnergia)
-    }
+function defender(nombre1, nombre2, ataque, energia, recuperacion) {    
+    energiaActual = energia - ataque
+    console.log(`${nombre1} recibio ${ataque} de daño por parte de ${nombre2} le queda ${energiaActual}`)
+    recargarEnergia(nombre1, energiaActual, recuperacion)
 }
 
-function recargarEnergia() {
-    nivelEnergia += 10;
+function recargarEnergia(nombre, energia, recuperacion) {
+    console.log(`${nombre} recargo su energia, actualmente tiene ${energia+recuperacion}`)
 }
+
+ataque(nombreHeroe1, nombreHeroe2, daño2, nivelEnergiaHeroe1, 5)
