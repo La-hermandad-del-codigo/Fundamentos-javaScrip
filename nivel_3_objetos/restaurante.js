90d07f982e0f819633baa9c40c38ef95ef1d5fd5
Jhefry09 <jhefrycabanillas@gmail.com>
2026-02-28 11:30:17 -0500

    crecion de el objeto cliente

---
+++ nivel_3_objetos/restaurante.js
@@ -0,0 +1,18 @@
+let cliente = {
+    nombre : "Carlitos",
+    dinero : 70,
+    hambre : 60
+}
+
+
+function ordenarComida(nom, com, pre) {
+    
+}
+
+function pagar(pre, dinero) {
+
+}
+
+function mostrarEstado(clien) {
+    
+}
